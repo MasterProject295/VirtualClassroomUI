e31efddb4c1082890dc1ec87954384b5458c2185
sai pranesh mukkala <saipranesh.mukkala@sjsu.edu>
2017-03-06 15:26:11 -0800

    Added walls for entry level

---
+++ Assets/_Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadNextScene() {
+        Debug.Log("You have clicked me ");
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
