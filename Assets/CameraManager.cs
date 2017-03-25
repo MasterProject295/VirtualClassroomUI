e27caf5db0536e958db36379ff8ee15d65b7e419
codepenman <harish.kv12@gmail.com>
2017-03-24 21:23:58 -0700

    Updated Capsule(Player) Prefeb and Class Room

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -9,10 +9,16 @@ public class CameraManager : NetworkBehaviour {
 	void Start () {
 
 		if (isLocalPlayer) {
-			
-			GetComponent<Camera>().enabled = true;
+			Debug.Log ("****I am Student***");
+			//Camera.main.gameObject.SetActive (false);
+			//GetComponent<Camera> ().enabled = true;
 			transform.position = new Vector3 (0f, 1.5f, 2.1f);
-			transform.rotation = Quaternion.Euler (0, 180, 0);
+			transform.rotation = Quaternion.Euler (0, -180 , 0);
+			Debug.Log (Camera.main.transform.rotation.x + " " + Camera.main.transform.rotation.y + " " + Camera.main.transform.rotation.z);
+			Camera.main.transform.position = transform.position;
+			Camera.main.transform.rotation = transform.rotation;
+			Debug.Log (Camera.main.transform.rotation.x + " " + Camera.main.transform.rotation.y + " " + Camera.main.transform.rotation.z);
+			//Camera.main.gameObject.SetActive(false);
 		}
 
 
