bd2a6a1fb91355f33d89e6d5ff49f00ca155aa0d
codepenman <harish.kv12@gmail.com>
2017-03-29 19:22:00 -0700

    FIxed Unity Remote 5 issue and added different views

---
+++ Assets/StudentView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Student : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {	
+		if (isServer) {
+			Debug.Log ("Initializing Instructor View");	
+			gameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
