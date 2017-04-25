7e2eb1f34d5311cfd293e70783c22d6513043ff5
akshaymathur90 <akshay.mathur@sjsu.edu>
2017-04-25 10:06:54 -0700

    GROOV-47 Destroy-Disable script for cup game object

---
+++ Assets/_Assets/_Scripts/DisableComponentCup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Leap.Unity.Interaction;
+
+public class DisableComponentCup : NetworkBehaviour {
+
+	private Rigidbody rb;
+	private BoxCollider boxCollider;
+	private InteractionBehaviour ib;
+	// Use this for initialization
+	void Start () {
+		
+		if (isClient)
+		{
+			Debug.Log("Disabling Components: Cup");
+			rb = GetComponent<Rigidbody>();
+			rb.detectCollisions = false;
+			rb.useGravity = false;
+			ib = GetComponent<InteractionBehaviour>();
+			ib.enabled = false;
+			boxCollider = GetComponentInChildren<BoxCollider>();	
+			boxCollider.enabled = false;
+			Destroy (boxCollider);
+			Destroy (ib);
+			Destroy(rb);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
