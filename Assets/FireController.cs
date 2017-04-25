7a93ec23b7ec443b208730e14955a84a2ce36b90
saipraneshm <saipranesh.mukkala@sjsu.edu>
2017-04-24 22:16:54 -0700

    GROOV-45 bunsen burner chain reaction code

---
+++ Assets/FireController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FireController : NetworkBehaviour {
+
+	protected bool fireplay = false;
+	public GameObject fire;
+	public GameObject smoke;
+	public GameObject metal;
+	public GameObject powderAnimation;
+	public GameObject mgo;
+	public GameObject cup;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		metal = GameObject.FindGameObjectWithTag ("metal");
+		cup = GameObject.FindGameObjectWithTag ("cup");
+	}
+
+	void OnMouseDown(){
+		
+
+		Debug.Log ("Mouse Clicked");
+
+		fireplay = !fireplay;
+		if (fireplay) {
+
+			fire = Instantiate (fire);
+			if (!fire.GetComponent<ParticleSystem> ().isPlaying) {
+				fire.GetComponent<ParticleSystem> ().Stop ();
+			}
+				
+			StartCoroutine(smokegenerator ());
+
+		} else {
+			if (fire.GetComponent<ParticleSystem> ().isPlaying) {
+				fire.GetComponent<ParticleSystem> ().Stop ();
+			}
+			Destroy (fire);
+
+			if (smoke.GetComponent<ParticleSystem> ().isPlaying) {
+				smoke.GetComponent<ParticleSystem> ().Stop ();
+				Destroy (smoke);
+			}
+			if (powderAnimation.GetComponent<ParticleSystem> ().isPlaying) {
+				powderAnimation.GetComponent<ParticleSystem> ().Stop ();
+				Destroy (powderAnimation);
+			}
+		
+		}
+
+	}
+
+	IEnumerator smokegenerator(){
+
+		yield return new WaitForSeconds (3);
+		smoke = Instantiate (smoke);
+		if (!smoke.GetComponent<ParticleSystem> ().isPlaying) {
+			smoke.GetComponent<ParticleSystem> ().Play ();
+		}
+
+		StartCoroutine(metalburning ());
+
+	}
+
+	IEnumerator metalburning(){
+
+		yield return new WaitForSeconds (3);
+
+		//Quaternion rotation = Quaternion.Euler (-90, 0, 0);
+		powderAnimation = Instantiate (powderAnimation, metal.transform.position,  Quaternion.identity);
+		if (!powderAnimation.GetComponent<ParticleSystem> ().isPlaying) {
+			powderAnimation.GetComponent<ParticleSystem> ().Play ();
+		}
+
+		StartCoroutine(metaldisappearing ());
+	}
+
+	IEnumerator metaldisappearing(){
+
+		yield return new WaitForSeconds (5);
+
+		if (powderAnimation.GetComponent<ParticleSystem> ().isPlaying) {
+			powderAnimation.GetComponent<ParticleSystem> ().Stop ();
+			Destroy (powderAnimation);
+		}
+			
+		mgo = Instantiate (mgo, metal.transform.position, Quaternion.identity);
+		Destroy (metal);
+	
+		if (smoke.GetComponent<ParticleSystem> ().isPlaying) {
+			smoke.GetComponent<ParticleSystem> ().Stop ();
+			Destroy (smoke);
+		}
+
+	}
+}
