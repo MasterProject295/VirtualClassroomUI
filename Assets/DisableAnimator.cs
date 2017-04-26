8f08908395bf23fabc3ab081a4d2d36b7f63e396
RekhaShankarReddy <rekha.shankar.1409@gmail.com>
2017-04-26 09:39:11 -0700

    GROOV-11 disabling animator component on all clients

---
+++ Assets/DisableAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableAnimator : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		if (!isServer) {
+			gameObject.GetComponent<Animator> ().enabled = false;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
